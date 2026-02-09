522c20ce01c462931f16a5c25da3c16347db05e1
EliasNiemi07 <niemielias07@gmail.com>
2026-02-09 13:56:15 +0200

    changes

---
+++ number.js
@@ -0,0 +1,23 @@
+const maxnum = document.querySelector("#maxnumber")
+const form = document.querySelector("#numbeform")
+const submit = document.querySelector("#numsubmit")
+let number = 0;
+let max = maxnum
+
+submit.addEventListener("click", function (event) {
+    event.preventDefault();
+    
+
+    number.textContent = "";
+    randonumber = Math.floor(Math.random() * max + 1);
+if (maxnum === "") {
+    number.textContent = "Input a number!"
+    return;
+}
+
+number.textContent = randonumber
+
+
+
+
+});
